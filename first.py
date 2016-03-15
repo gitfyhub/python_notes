2c8963457067b5bf30db81d5737bf425aed6fa61
Faisal Younus <fyounus99@gmail.com>
2016-03-15 01:18:25 -0500

    initial code commit

---
+++ first.py
@@ -0,0 +1,40 @@
+foods = ['apples','tuna','oranges','fish'];
+
+for f in foods:
+    print(f)
+
+
+
+'''
+#name = "Tonny D"
+name = "bucky"
+
+if name is "Bucky":
+    print("Hey there Bucky")
+elif name is "Lucy":
+    print ("What's going on Lucy")
+elif name is "Sammy":
+    print("What is going Sammy")
+else:
+    print("Please sign up for the site (inside else)")
+'''
+
+
+
+
+'''
+#-------------------
+age = 13
+
+if age < 21:
+    print("No biyani for you!")
+elif age > 21:
+    print("Biryani for you since you are greater than 21")
+
+#--------------------
+'''
+
+'''
+print('Big Momma = Safa')
+print('Thiiiize')
+'''
